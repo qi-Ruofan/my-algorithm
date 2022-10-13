ba2a193a22f830ea267b1a986f724c2799c73524
qrf <qirf@dtinsure.com>
2022-10-13 23:01:11 +0800

    环境搭建

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { routate1, routate2 } from './question/01array-route'
+console.log(routate1([1, 2, 3, 4, 5], 2));
+console.log(routate2([1, 2, 3, 4, 5], 2));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.ts'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'development',
+    }),
+  ],
+  devServer: {
+    static: './dist'
+  }
+}
